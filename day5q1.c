c4aa20849c357569cafc751b36dd82e199f0d4de
Dhruvg225 <114644214+Dhruvg225@users.noreply.github.com>
2022-12-20 19:34:16 +0530

    Add files via upload

---
+++ day5q1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int number(char );
+void main(){
+    int i,j,n;
+    char a;
+    printf("Enter starting char:");
+    scanf(" %c",&a);
+    printf("%d\n",a);
+    n=number(a);
+    printf("%d\n",n);
+    for(i=1;i<=n;i++){
+        for(j=n-i+1;j<=n;j++){
+
+            printf("%c",'A'+j-1);
+        }
+        printf("\n");
+
+    }
+
+
+}
+int number(char a){
+    int count=0,i;
+    for(i=65;i<=a;i++){
+        count++;
+    }
+    return count;
+
+
+}
